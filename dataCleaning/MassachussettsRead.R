9d1294481670ab1950eb9abedea4c5bcc4511aba
Lief <williamlief@gmail.com>
2019-07-07 10:03:39 -0700

    Fixed na conversion warnings by stripping commas and '-'. Filtered out state total rows. Cleaned up paths

--- dataCleaning/MassachussettsRead.R
+++ dataCleaning/MassachussettsRead.R
@@ -1,23 +1,19 @@
-
 # Setup ------------------------------------------------------------------------
-
 
 library(dplyr)
 library(readxl)
 library(readr)
 
-
 source("setup.r")
 
-path <- setpath("Massachusettes")
+path <- setpath("Massachusetts")
 
 # Read -------------------------------------------------------------------------
-
 
 list <- list.files(path = path, pattern = "*.xlsx", full.names = TRUE)
 files <- lapply(list, read_excel, na = c("NA","NI","NR"), skip = 1) 
 
-years <- c(2014:2017,2013)
+years <- c(2014:2017, 2013) # 'files' is in order of file names
 for(i in 1:length(files)) {
   files[[i]]$year <- years[i]
 }
@@ -27,23 +23,28 @@ df <- bind_rows(files)
 names(df) <- c("name", "localid", "et", "evaluated", "percentEvaluated",
                "e4", "e3", "e2", "e1", "year") 
 
+
+# Clean ------------------------------------------------------------------------
+
 convertNumber <- function(e, evaluated) {
-  as.numeric(e) / 100 * as.numeric(evaluated)
+  round(as.numeric(gsub("-", NA, e)) / 100 * evaluated, 0)
 }
 
 Massachusetts <- df %>%
-  mutate(name = tolower(name),
-         es = ifelse(e1 == "-" & e2 == "-" & e3 == "-" & e4 == "-", evaluated, 0),
-         e4 = convertNumber(e4, evaluated),
-         e3 = convertNumber(e3, evaluated),
-         e2 = convertNumber(e2, evaluated),
-         e1 = convertNumber(e1, evaluated),
-         
-         eu = as.numeric(et) - as.numeric(evaluated)) %>% 
-  
-  mutate_at(vars(e1, e2, e3, e4), funs(round(.,0))) %>% 
-  
+  mutate(
+    name = tolower(name),
+    evaluated = as.numeric(gsub(",", "", evaluated)), 
+    et = as.numeric(gsub(",", "", et)),
+    es = ifelse(e1 == "-" & e2 == "-" & e3 == "-" & e4 == "-", evaluated, 0),
+    eu = et - evaluated
+    ) %>% 
+  mutate_at(
+    vars(e1, e2, e3, e4), 
+    list(~convertNumber(., evaluated))
+    ) %>% 
   select("name", "localid", "year", "et", "eu", "es",
-         "e4", "e3", "e2", "e1")
+         "e4", "e3", "e2", "e1") %>% 
+  filter(name != "state totals") %>% 
+  arrange(localid, year)
 
-write_csv(Massachusetts, "MassachusettsEval.csv")
+write_csv(Massachusetts, "CleanData/MassachusettsEval.csv")
