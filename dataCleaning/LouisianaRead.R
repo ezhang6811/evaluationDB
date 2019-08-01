4bc593e9f410743c6a9ced3df52a92eeff942e23
ezhang6811 <eric.zhang6811@gmail.com>
2019-07-31 18:38:30 -0700

    Update LouisianaRead.R

--- dataCleaning/LouisianaRead.R
+++ dataCleaning/LouisianaRead.R
@@ -32,32 +32,32 @@ d1 <- read_fun(ranges = list("A3:A74", "G3:J74"),
                year = 2013)
 
 # 2013-14
-d2 <- read_fun(ranges = list(), 
-               file = "",
-               sheet = "",
-               top_row_drop = 0, 
-               year = )
+d2 <- read_fun(ranges = list("A3:A74", "J3:M74"), 
+               file = "2013-2014-compass-teacher-results-by-parish.xlsx",
+               sheet = "Teacher Data by Parish",
+               top_row_drop = 2, 
+               year = 2014)
 
 # 2014-15
-d3 <- read_fun(ranges = list(), 
-               file = "",
-               sheet = "",
-               top_row_drop = 0, 
-               year = )
+d3 <- read_fun(ranges = list("A6:E77"), 
+               file = "2014-2015-compass-teacher-results-by-lea.xlsx",
+               sheet = "Teacher Data by District",
+               top_row_drop = 2, 
+               year = 2015)
 
 # 2015-16
-d4 <- read_fun(ranges = list(), 
-               file = "",
-               sheet = "",
-               top_row_drop = 0, 
-               year = )
+d4 <- read_fun(ranges = list("B6:F77"), 
+               file = "2015-2016-compass-teacher-results-by-district.xlsx",
+               sheet = "district list",
+               top_row_drop = 2, 
+               year = 2016)
 
 # 2016-17
-d5 <- read_fun(ranges = list(), 
-               file = "",
-               sheet = "",
-               top_row_drop = 0, 
-               year = )
+d5 <- read_fun(ranges = list("B6:F79"), 
+               file = "2016-2017-compass-teacher-results-by-district-and-school.xlsx",
+               sheet = "State-District Level",
+               top_row_drop = 2, 
+               year = 2017)
 
 # 2017-18
 d6 <- read_fun(ranges = list("B6:F78"), 
